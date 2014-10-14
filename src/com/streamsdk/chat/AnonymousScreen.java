67a5db9e637ca10d51dd9a439b57562f3b2d086e
support@streamsdk.com <coding560805>
2014-10-14 18:28:20 +0100

    ano screen/layout

---
+++ src/com/streamsdk/chat/AnonymousScreen.java
@@ -0,0 +1,14 @@
+package com.streamsdk.chat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnonymousScreen extends Activity{
+	
+	 public void onCreate(Bundle savedInstanceState){
+		 
+		 super.onCreate(savedInstanceState);
+		 
+	 }
+
+}
